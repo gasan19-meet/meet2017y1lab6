54264d9cdb00e0ee40b443714a8b62c5cbd6fb67
gasan19-meet <gasan19@meet.mit.edu>
2017-07-31 17:39:03 +0200

    Stopping here

--- turtlefun2.py
+++ turtlefun2.py
@@ -1,10 +1,47 @@
-up_arrow='up'
-left_arrow='left'
-down_arrow='down'
-right_arrow='right'
-spacebar='space'
-up=0
-left=1
-down=2
-right=three
-direction=up
+
+import turtle
+
+UP_ARROW='Up'
+LEFT_ARROW='Left'
+DOWN_ARROW='Down'
+RIGHT_ARROW='Right'
+
+UP=0
+LEFT=1
+DOWN=2
+RIGHT=3
+
+direction= UP
+
+def up():
+    global direction
+    direction=UP
+    print ('you pressed UP')
+
+
+def down():
+    global direction
+    direction=DOWN
+    print('you pressed DOWN')
+
+
+def left():
+    global direction
+    direction=LEFT
+    print ('you pressed LEFT')
+
+def right():
+    global direction
+    direction =RIGHT
+    print('RIGHT')
+
+
+turtle.onkeypress(up, UP_ARROW)
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(right, RIGHT_ARROW)
+turtle.onkeypress(left, LEFT_ARROW)
+turtle.listen()
+
+
+
+
